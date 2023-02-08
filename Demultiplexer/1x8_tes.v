0cb5b6dc9861c368119a9e23d781d41aaf7f53ef
Adhi-cyber <112969103+Adhi-cyber@users.noreply.github.com>
2023-02-08 09:07:33 +0530

    Create 1x8_tes.v

---
+++ Demultiplexer/1x8_tes.v
@@ -0,0 +1,26 @@
+module test_demux;
+reg [2:0]S;
+reg A;
+wire [7:0]Y;
+demux_1_8 mydemux(.y(Y), .a(A), .s(S));
+initial begin
+A=1;
+S=3'd5;
+#30;
+A=0;
+S=3'd1;
+#30;
+A=1;
+S=3'd1;
+#30;
+
+S=3'd6;
+#30;
+
+S=3'd0;
+
+#30;
+$finish;
+
+end
+endmodule
