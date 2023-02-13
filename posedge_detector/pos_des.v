896a1ec47eff04476d5b25618a4993524952bdf5
Adhi-cyber <112969103+Adhi-cyber@users.noreply.github.com>
2023-02-13 20:29:19 +0530

    Add files via upload

---
+++ posedge_detector/pos_des.v
@@ -0,0 +1,10 @@
+module pos_edgedetect ( input sig, input clk, output pe);          
+
+reg   signal;                          
+
+always @ (posedge clk) begin
+    signal <= sig;
+  end
+ 
+assign pe = sig & ~signal;            
+endmodule 
